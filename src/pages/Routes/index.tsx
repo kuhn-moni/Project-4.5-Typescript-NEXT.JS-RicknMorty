0f1c28bf87855669f6db638d978fab4ed5026b37
kuhn-moni <kuhn.moonay@gmail.com>
2023-09-12 13:25:29 +0200

    first commit

---
+++ src/pages/Routes/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Routes = (props: Props) => {
+  return <h1>Route testing</h1>;
+};
+
+export default Routes;
---
+++ src/pages/Routes/page1.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Page1({}: Props) {
+  return <h1>Page 1</h1>;
+}
+
+export default Page1;

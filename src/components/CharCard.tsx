7d2a98e9e15c3d531a0e8e73e2631836e552d873
kuhn-moni <kuhn.moonay@gmail.com>
2023-09-13 16:48:08 +0200

    getServerSideProp working, rendering in CharCard

--- src/components/CharCard.tsx
+++ src/components/CharCard.tsx
@@ -1,19 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import styled from "styled-components";
-
-const CharContent = styled.a`
-  display: block;
-  text-decoration: none;
-  color: #000;
-`;
 
 const CharCard = ({ character }) => {
   return (
-    <CharContent href={`/character/${character.id}`} prefetch={false}>
+    <div href={`/character/${character.id}`} prefetch={false}>
       <Image loader={() => character.image} src={character.image} unoptimized width={300} height={300} alt={character.name} />
       <h2>{character.name}</h2>
-    </CharContent>
+    </div>
   );
 };
 
